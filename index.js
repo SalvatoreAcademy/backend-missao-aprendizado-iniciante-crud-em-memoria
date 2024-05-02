e5d6ed6bcac3c0d21152ae2082b9d4d322d1583f
Paulo Salvatore <salvatoregames@gmail.com>
2024-05-02 02:17:11 +0000

    Status HTTP adicionados em POST e PUT

--- index.js
+++ index.js
@@ -38,19 +38,19 @@ app.post('/personagem', function (req, res) {
 
   // Checar se o `nome` está presente no body
   if (!novoItem) {
-    return res.send('Corpo da requisição deve conter a propriedade `nome`.')
+    return res.status(400).send('Corpo da requisição deve conter a propriedade `nome`.')
   }
 
   // Checar se o novoItem está na lista ou não
   if (lista.includes(novoItem)) {
-    return res.send('Esse item já existe na lista.')
+    return res.status(409).send('Esse item já existe na lista.')
   }
 
   // Adicionamos na lista
   lista.push(novoItem)
 
   // Exibimos uma mensagem de sucesso
-  res.send('Item adicionado com sucesso: ' + novoItem)
+  res.status(201).send('Item adicionado com sucesso: ' + novoItem)
 })
 
 // Endpoint Update [PUT] /personagem/:id
@@ -66,12 +66,12 @@ app.put('/personagem/:id', function (req, res) {
 
   // Checar se o `nome` está presente no body
   if (!novoItem) {
-    return res.send('Corpo da requisição deve conter a propriedade `nome`.')
+    return res.status(400).send('Corpo da requisição deve conter a propriedade `nome`.')
   }
 
   // Checar se o novoItem está na lista ou não
   if (lista.includes(novoItem)) {
-    return res.send('Esse item já existe na lista.')
+    return res.status(409).send('Esse item já existe na lista.')
   }
 
   // Atualizamos na lista o novoItem pelo ID - 1
